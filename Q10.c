5e0b0e56ebb197102242385d8a77918e1f9dd60b
abhishekk111 <109384287+abhishekk111@users.noreply.github.com>
2022-07-16 12:17:52 -0700

    Delete Q10.c

--- Q10.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-int a,b,c;
-printf("Enter any date month year in DD/MM/YYYY format:-");
-scanf("%d/%d/%d",&a,&b,&c);
-printf("Day-%d,Month-%d,Year-%d",a,b,c);
-return 0;
-}
